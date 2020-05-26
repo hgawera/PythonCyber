0cb0eb2ac2000751f40933ba787053d151936c39
hgawera <43831297+hgawera@users.noreply.github.com>
2020-05-26 15:48:40 -0400

    Network Scanner Enhanced
    For internal IP range from 192.168.1.1/24

--- network_scanner.py
+++ network_scanner.py
@@ -2,17 +2,26 @@
 
 import scapy.all as scapy
 
+
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-    print("IP\t\t\t\t\tMAC Address\n-------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+    clients_list= []
+    for element in answered_list:
+        client_dict = {"IP": element[1].psrc, "MAC": element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
 
-    for element in  answered_list:
-        print(element[1].psrc + "\t\t\t\t" + element[1].hwsrc)
-        print("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+
+def print_result(results_list):
+    print("IP\t\t\t\t\tMAC Address\n-------------------------------------------------------------------------")
+    for result in results_list:
+        print(result["IP"] + "\t\t\t\t" + result["MAC"] + "\t\t")
 
 
-scan("192.168.1.1/24")
+
+scan_result = scan("192.168.1.1/24")
+print_result(scan_result)
