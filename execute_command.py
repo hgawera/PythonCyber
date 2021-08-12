3c288796713270913e19023243fb6014066581d8
hgawera <43831297+hgawera@users.noreply.github.com>
2021-08-12 21:43:46 +0100

    message hack added

--- execute_command.py
+++ execute_command.py
@@ -2,7 +2,14 @@ import subprocess
 
 # Subprocess allows us to execute system commands
 
-command = "msg * You have been Hacked bro"
+# command = "wmic product get name"
+message = "'You have been hacked!'"
+commandone = "PowerShell -Command "
+commandtwo = '"Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]'
+commandthree = '::Show(' + str(message) + ')"'
+
+commands = commandone + commandtwo + commandthree
 # Popen executes command and keeps programme running (does not wait for command to finish)
-subprocess.Popen(command, shell=True)
+print(commands)
+subprocess.Popen(commands, shell=True)
 # So invoking the shell invokes a program of the user's choosing and is platform-dependent.
