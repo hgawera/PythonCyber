f24c3a8cc44c4007bb2e4d3a411ddf10311e0d51
hgawera <43831297+hgawera@users.noreply.github.com>
2021-08-12 20:44:28 +0100

    Simple windows message

--- execute_command.py
+++ execute_command.py
@@ -2,5 +2,7 @@ import subprocess
 
 # Subprocess allows us to execute system commands
 
-command = ""
-subprocess.Popen() #Popen executes command and keeps programme running (does not wait for command to finish)
+command = "msg * You have been Hacked bro"
+# Popen executes command and keeps programme running (does not wait for command to finish)
+subprocess.Popen(command, shell=True)
+# So invoking the shell invokes a program of the user's choosing and is platform-dependent.
