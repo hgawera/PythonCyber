2146ac6e15d1107bc084b711446c37846935d538
hgawera <43831297+hgawera@users.noreply.github.com>
2021-08-12 20:38:44 +0100

    execute_command

---
+++ execute_command.py
@@ -0,0 +1,6 @@
+import subprocess
+
+# Subprocess allows us to execute system commands
+
+command = ""
+subprocess.Popen() #Popen executes command and keeps programme running (does not wait for command to finish)
